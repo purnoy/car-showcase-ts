061f1449c47a0c314c0839d5cf6278efaa277aa2
Purnot <purnoy.cc@gmail.com>
2023-07-17 15:34:07 +0600

    starting the navigation and footer within the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Hero from '@/components/Hero'
+
+import { Hero } from '@/components';
 import Image from 'next/image'
 
 export default function Home() {
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+import CustomButton from './CustomButton';
+import Image from 'next/image';
 
 const Hero = () => {
+    const handleScroll = () =>{
+
+    }
   return (
-    <div>
-      Hero
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+            Find, Book or Rent a car quickly and easily
+        </h1>
+        <p className="hero__subtitle">
+            Strealine your car rental experience with our effortless booking process.
+        </p>
+        <CustomButton
+        title = "Explore Cars"
+        containerStyles="bg-primary-blue text-white py-2 px-8 hover: rounded-full mt-10"
+        handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className='object-contain'/>
+            <div className='hero__image-overlay'/>
+        </div>
+      </div>
     </div>
   )
 }
